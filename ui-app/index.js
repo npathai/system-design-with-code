7c319c00cfd7e0cf6432249e05380061e16082d4
Narendra Pathai <narendra.pathai@gmail.com>
2020-05-30 07:10:20 -0400

    Create basic working ui app

--- ui-app/index.js
+++ ui-app/index.js
@@ -1,5 +1,24 @@
+
 function shortenUrl() {
     console.log("Submit event fired")
+    $.ajax({
+        url: "http://localhost:4321/shorten",
+        type: "POST",
+        data: JSON.stringify({longUrl: "http://google.com"}),
+        success: function(data) {
+            var row = document.createElement("TR");                 // Create a <li> node
+            var longCol = document.createElement("TD");                 // Create a <li> node
+            var shortCol = document.createElement("TD");                 // Create a <li> node
+            var textnode = document.createTextNode(data["longUrl"]);         // Create a text node
+            var textnode2 = document.createTextNode(data["id"]);         // Create a text node
+            longCol.appendChild(textnode)
+            shortCol.appendChild(textnode2)
+            row.appendChild(longCol)
+            row.appendChild(shortCol)
 
+            document.getElementById("urls").appendChild(row)
+
+        }
+    });
     return false
 }
