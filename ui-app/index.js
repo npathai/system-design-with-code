03376b464d976cddb130b1e6e1252d2dd1289dba
Narendra Pathai <narendra.pathai@gmail.com>
2020-06-06 05:34:00 -0400

    Added the short url in form of link in ui app, so that it is easy to access

--- ui-app/index.js
+++ ui-app/index.js
@@ -10,9 +10,11 @@ function shortenUrl() {
             var longCol = document.createElement("TD");                 // Create a <li> node
             var shortCol = document.createElement("TD");                 // Create a <li> node
             var textnode = document.createTextNode(data["longUrl"]);         // Create a text node
-            var textnode2 = document.createTextNode("http://localhost:4321/" + data["id"]);         // Create a text node
+            var linkNode = document.createElement("A");
+            linkNode.href = "http://localhost:4321/" + data["id"];
+            linkNode.innerText = "http://localhost:4321/" + data["id"];
             longCol.appendChild(textnode)
-            shortCol.appendChild(textnode2)
+            shortCol.appendChild(linkNode)
             row.appendChild(longCol)
             row.appendChild(shortCol)
 
