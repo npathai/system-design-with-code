9c88c9daeceea508a9320a6d352a1d5095aeef01
Narendra Pathai <narendra.pathai@gmail.com>
2020-06-09 13:54:21 -0400

    Created basic React app to write ui-app in React

---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+    return <h1>URL Shortener</h1>
+}
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);
