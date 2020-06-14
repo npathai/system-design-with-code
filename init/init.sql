105dc5247b805e99540836763eb31fecee1ac058
Narendra Pathai <narendra.pathai@gmail.com>
2020-06-14 07:15:14 -0400

    Resolved issue of SQL Constraint violation for ID of url, because MySQL VARCHAR is case insensitive. Modified to to utf-8-bin to make it case sensitive"

--- init/init.sql
+++ init/init.sql
@@ -6,8 +6,12 @@ GRANT ALL PRIVILEGES ON urldb.* TO 'root'@'localhost';
 
 USE short_url_generator;
 
+/*
+ MySQL VARCHAR is not case-sensitive, so it cased integrity violation because it considered AAAAa and AAAAA as same
+ values. That's why  SET utf8 COLLATE utf8_bin has been added which makes it case insensitive.
+ */
 CREATE TABLE redirection(
-    id VARCHAR(10) NOT NULL,
+    id VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
     long_url VARCHAR(500) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     expiry_at TIMESTAMP NOT NULL,
