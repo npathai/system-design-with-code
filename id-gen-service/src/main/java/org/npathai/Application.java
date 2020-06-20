b99a43e33c940550d6c34ec5dc2036a05c2eb8cc
Narendra Pathai <narendra.pathai@gmail.com>
2020-06-20 04:51:28 -0400

    Configured log4j2 configurations for proper logging. Root log level set to WARN and all loggers for org.npathai have been configured at ALL level for better understanding of flow

--- id-gen-service/src/main/java/org/npathai/Application.java
+++ id-gen-service/src/main/java/org/npathai/Application.java
@@ -1,10 +1,15 @@
 package org.npathai;
 
 import io.micronaut.runtime.Micronaut;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Application {
+    private static final Logger LOG = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
+        LOG.info("Starting application");
+
         Micronaut.run(Application.class);
     }
 }
