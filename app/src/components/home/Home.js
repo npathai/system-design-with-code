2e9e98d685bcef8d863981a1407c238112793f6a
Narendra Pathai <narendra.pathai@gmail.com>
2020-06-24 14:46:11 -0400

    Renamed component directory

--- app/src/components/home/Home.js
+++ app/src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Shortener from "../short-url-generator/Shortener";
-import RedirectionHistory from "../urls-by-user/RedirectionHistory";
+import RedirectionHistory from "../redirection-history/RedirectionHistory";
 import {AuthContext} from "../../context/AuthContext";
 
 class Home extends React.Component {
