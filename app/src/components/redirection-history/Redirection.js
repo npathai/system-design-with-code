5b2ed1fbc6d1f50fb842651f92ef05b7ab0acdcf
Narendra Pathai <narendra.pathai@gmail.com>
2020-06-30 14:27:53 -0400

    Add support for showing click count in redirection history

--- app/src/components/redirection-history/Redirection.js
+++ app/src/components/redirection-history/Redirection.js
@@ -1,6 +1,35 @@
 import React from 'react'
+import {ActionTypes as types} from "../../constants";
+import {connect} from 'react-redux'
 
 class Redirection extends React.Component {
+
+    componentDidMount() {
+        let headers = {
+            'Content-Type': 'application/json',
+            "Authorization": this.props.tokenType + " " + this.props.accessToken
+        }
+
+        fetch("http://localhost:4000/user/analytics/" + this.props.data.id, {
+            method: 'GET',
+            headers: headers,
+            mode: 'cors'
+        })
+            .then(res => {
+                if (res.status !== 200) {
+                    throw new Error("Error in fetching redirection history")
+                }
+                return res.json()
+            })
+            .then(res => {
+                console.log(res)
+                dispatch({type: types.RECEIVED_REDIRECTION_HISTORY_SUCCESS, data: res})
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch({type: types.RECEIVED_REDIRECTION_HISTORY_FAILURE, data: {errorMessage: getErrorMessage(err)}})
+            })
+    }
 
     render() {
         return (
@@ -13,4 +42,9 @@ class Redirection extends React.Component {
     }
 }
 
-export default Redirection
+export default connect((state, props) => {
+    return {
+        tokenType: state.auth.tokenType,
+        accessToken: state.auth.accessToken
+    }
+})(Redirection)
