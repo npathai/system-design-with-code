948e7a12ef5239b1a1e8e7aa67971a3dc6f04683
Narendra Pathai <narendra.pathai@gmail.com>
2020-09-04 15:19:51 -0400

    Added tests for Login endpoint and access token containing uid

---
+++ user-service/src/test/java/login/LoginEndpointTest.java
@@ -0,0 +1,122 @@
+package login;
+
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
+import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
+import io.micronaut.security.token.jwt.signature.secret.SecretSignatureConfiguration;
+import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.test.annotation.MockBean;
+import io.reactivex.Flowable;
+import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.npathai.dao.DataAccessException;
+import org.npathai.dao.UserDao;
+import org.npathai.model.User;
+
+import javax.inject.Inject;
+import java.text.ParseException;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@MicronautTest
+public class LoginEndpointTest {
+
+    @Inject
+    @Client(value = "/")
+    RxHttpClient httpClient;
+
+    @Inject
+    UserDao userDao;
+
+    @Inject
+    SecretSignatureConfiguration secretSignatureConfiguration;
+
+    @Test
+    public void returnsStatus200() throws DataAccessException {
+        HttpResponse<BearerAccessRefreshToken> authenticationResponse = attemptLogin();
+
+        assertThat(authenticationResponse.getStatus().getCode()).isEqualTo(HttpStatus.OK.getCode());
+    }
+
+    private HttpResponse<BearerAccessRefreshToken> attemptLogin() throws DataAccessException {
+        User user = createRootUser();
+        when(userDao.getUserByName("root")).thenReturn(java.util.Optional.of(user));
+
+        Flowable<HttpResponse<BearerAccessRefreshToken>> response = httpClient.exchange(
+                HttpRequest.create(HttpMethod.POST, "/login")
+                        .body(new LoginRequest("root", "root")),
+                BearerAccessRefreshToken.class);
+        return response.blockingFirst();
+    }
+
+    @Test
+    public void accessTokenContainsUID() throws ParseException, DataAccessException {
+        HttpResponse<BearerAccessRefreshToken> authenticationResponse = attemptLogin();
+
+        BearerAccessRefreshToken token = authenticationResponse.body();
+        assertThat(token).isNotNull();
+
+        JWT jwt = JWTParser.parse(token.getAccessToken());
+        assertThat(jwt).isNotNull();
+        JWTClaimsSet jwtClaimsSet = jwt.getJWTClaimsSet();
+        assertThat(jwtClaimsSet.getClaim("uid").toString()).isNotBlank();
+    }
+
+    @NotNull
+    private User createRootUser() {
+        User user = new User();
+        user.setUsername("root");
+        // FIXME change to use of bcrypt library
+        user.setPassword("$2a$09$C75xhHFSNwj0GV6STPWTqOgZ2qYpvH88QxGXbxWUF/kC0qgfJAEI.");
+        user.setEmail("root@github.com");
+        user.setId(UUID.randomUUID().toString());
+        return user;
+    }
+
+    @MockBean(UserDao.class)
+    public UserDao createUserDao() {
+        return Mockito.mock(UserDao.class);
+    }
+
+    public static class LoginRequest {
+        private String username;
+        private String password;
+
+        public LoginRequest() {
+
+        }
+
+        public LoginRequest(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
