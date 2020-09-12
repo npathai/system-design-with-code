dd9ccb53122d19e964800bcd5957754d96957338
Narendra Pathai <narendra.pathai@gmail.com>
2020-09-12 13:44:57 -0400

    Added fields in Show entity

--- bookmyshow/application/src/main/java/org/npathai/cinemahall/show/Show.java
+++ bookmyshow/application/src/main/java/org/npathai/cinemahall/show/Show.java
@@ -1,5 +1,18 @@
 package org.npathai.cinemahall.show;
 
+import org.npathai.cinemahall.auditorium.AuditoriumId;
+import org.npathai.cinemahall.seat.Seat;
+import org.npathai.movie.MovieId;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
 public class Show {
+    private long id;
+    private AuditoriumId auditoriumId;
+    private List<Seat> seats;
+    private MovieId movieId;
+    private ZonedDateTime startTime;
+
 
 }
