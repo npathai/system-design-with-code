2f5947510fed04c9213846653669d800160495b5
Narendra Pathai <narendra.pathai@gmail.com>
2020-09-16 23:07:34 -0400

    Checks shows in all auditoriums of cinema hall for a movie id

--- bookmyshow/application/src/test/java/org/npathai/usecases/GetShowsByMovieTest.java
+++ bookmyshow/application/src/test/java/org/npathai/usecases/GetShowsByMovieTest.java
@@ -2,10 +2,12 @@ package org.npathai.usecases;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.npathai.cinemahall.CinemaHall;
 import org.npathai.cinemahall.CinemaHallBuilder;
 import org.npathai.cinemahall.CinemaHallDao;
+import org.npathai.cinemahall.auditorium.Auditorium;
+import org.npathai.cinemahall.auditorium.AuditoriumId;
 import org.npathai.cinemahall.show.Show;
 import org.npathai.cinemahall.show.ShowBuilder;
 import org.npathai.cinemahall.show.ShowDao;
@@ -17,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -29,22 +32,29 @@ class GetShowsByMovieTest {
     private static final CinemaHall CINEMA_HALL_WITH_2_AUDITORIUMS
             = CinemaHallBuilder.aCinemaHall()
                 .anAuditorium().build()
-//                .anAuditorium().build()
+                .anAuditorium().build()
             .build();
 
     private static final Show HARRY_POTTER_SHOW_1 = ShowBuilder.aShow()
             .withAuditoriumId(CINEMA_HALL_WITH_2_AUDITORIUMS.getAudis().get(0).getId())
             .withMovieId(HARRY_POTTER_MOVIE_ID)
             .build();
+    private static final Show HARRY_POTTER_SHOW_2 = ShowBuilder.aShow()
+            .withAuditoriumId(CINEMA_HALL_WITH_2_AUDITORIUMS.getAudis().get(1).getId())
+            .withMovieId(HARRY_POTTER_MOVIE_ID)
+            .build();;
 
     private GetShowsByMovie getShowsByMovie;
+    @Mock
     private CinemaHallDao cinemaHallDao;
+    @Mock
     private ShowDao showDao;
+    @Captor
+    private ArgumentCaptor<AuditoriumId> auditoriumIdArgumentCaptor;
 
     @BeforeEach
     public void setUp() {
-        cinemaHallDao = Mockito.mock(CinemaHallDao.class);
-        showDao = Mockito.mock(ShowDao.class);
+        MockitoAnnotations.initMocks(this);
         getShowsByMovie = new GetShowsByMovie(cinemaHallDao, showDao);
     }
 
@@ -73,16 +83,19 @@ class GetShowsByMovieTest {
     @Test
     public void returnsShowsOfAllCinemaHallsInCurrentCityShowingMovie() {
         when(cinemaHallDao.getCinemaHalls(CURRENT_CITY_ID)).thenReturn(List.of(CINEMA_HALL_WITH_2_AUDITORIUMS));
-        when(showDao.getShows(eq(CINEMA_HALL_WITH_2_AUDITORIUMS.getAudis().get(0).getId()), eq(HARRY_POTTER_MOVIE_ID)))
-                .thenReturn(List.of(HARRY_POTTER_SHOW_1));
-
-        // TODO check how to train mockito for this type of requirement
-//        when(showDao.getShows(CINEMA_HALL_WITH_2_AUDITORIUMS.getAudis().get(1).getId(), HARRY_POTTER_MOVIE_ID))
-//                .thenReturn(Collections.emptyList());
+        when(showDao.getShows(any(AuditoriumId.class), any(MovieId.class)))
+                .thenReturn(List.of(HARRY_POTTER_SHOW_1))
+                .thenReturn(List.of(HARRY_POTTER_SHOW_2));
 
         Shows shows = getShowsByMovie.execute(HARRY_POTTER_MOVIE_ID, CURRENT_CITY_ID);
 
-        Map<CinemaHall, List<Show>> expected = Map.of(CINEMA_HALL_WITH_2_AUDITORIUMS, List.of(HARRY_POTTER_SHOW_1));
+        Map<CinemaHall, List<Show>> expected = Map.of(CINEMA_HALL_WITH_2_AUDITORIUMS, List.of(HARRY_POTTER_SHOW_1, HARRY_POTTER_SHOW_2));
         assertThat(shows.getAll()).isEqualTo(expected);
+        verify(showDao, Mockito.times(2)).getShows(auditoriumIdArgumentCaptor.capture(), eq(HARRY_POTTER_MOVIE_ID));
+
+        assertThat(auditoriumIdArgumentCaptor.getAllValues()).isEqualTo(List.of(
+                CINEMA_HALL_WITH_2_AUDITORIUMS.getAudis().get(0).getId(),
+                CINEMA_HALL_WITH_2_AUDITORIUMS.getAudis().get(1).getId()
+        ));
     }
 }
