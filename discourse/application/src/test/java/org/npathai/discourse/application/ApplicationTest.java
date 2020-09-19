bcde4ac22a97371b3882ecdd50afb783bddd90b4
Narendra Pathai <narendra.pathai@gmail.com>
2020-09-18 23:41:31 -0400

    Created template for Discourse Q&A site.

---
+++ discourse/application/src/test/java/org/npathai/discourse/application/ApplicationTest.java
@@ -0,0 +1,11 @@
+package org.npathai.discourse.application;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    public void applicationRuns() {
+        Application.main(new String[] {});
+    }
+}
