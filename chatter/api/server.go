084cc4fcef13ffb924651ac92e7222ced4d90e5b
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-18 08:04:04 -0400

    Basic template for starting and stopping chatter api server

---
+++ chatter/api/server.go
@@ -0,0 +1,11 @@
+package api
+
+import "fmt"
+
+func NewServer() {
+	fmt.Println("Server started")
+}
+
+func StopServer() {
+	fmt.Println("Server stopped")
+}
--- chatter/main.go
+++ chatter/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/npathai/chatter/api"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("Welcome to Chatter, the single place for all your communications")
+	api.NewServer()
+	api.InitApi()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block on channel read
+	<- ch
+	api.StopServer()
 }
