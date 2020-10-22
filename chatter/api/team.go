826a9151714aae7e25a8e70804166b484796a4de
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-22 00:02:09 -0400

    Resolved compilation issue

--- chatter/api/team.go
+++ chatter/api/team.go
@@ -10,7 +10,7 @@ func InitTeam(router *mux.Router) {
 	fmt.Println("Initializing team api routes")
 
 	sr := router.PathPrefix("/teams").Subrouter()
-	sr.HandleFunc("/create_from_signup", createTeamFromSignUp())
+	sr.HandleFunc("/create_from_signup", createTeamFromSignUp)
 }
 
 func createTeamFromSignUp(w http.ResponseWriter, r *http.Request) {
