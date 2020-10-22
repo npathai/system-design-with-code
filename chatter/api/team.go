717f9dfc101131241e58296027a51c98e91b8577
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-21 23:56:01 -0400

    Added team signup api

---
+++ chatter/api/team.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func InitTeam(router *mux.Router) {
+	fmt.Println("Initializing team api routes")
+
+	sr := router.PathPrefix("/teams").Subrouter()
+	sr.HandleFunc("/create_from_signup", createTeamFromSignUp())
+}
+
+func createTeamFromSignUp(w http.ResponseWriter, r *http.Request) {
+
+}
